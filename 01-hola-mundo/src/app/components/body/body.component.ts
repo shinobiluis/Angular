b1f5e6e56f1c4b091744bf22d4d3988773c80344
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-08-08 19:48:10 -0500

    terminamos el primer ejemplo con varios componentes de ejmplo

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 export class BodyComponent {
-
+    frase: any = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad.',
+        autor: 'Ben Parker'
+    };
+    mostrar = true;
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octopus'];
 }
