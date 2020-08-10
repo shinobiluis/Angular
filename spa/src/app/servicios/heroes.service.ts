47239add4ba93bfed785c5ae63ff170b63f4bedd
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-08-09 20:37:28 -0500

    Se coloco un componente llamado buscador

--- spa/src/app/servicios/heroes.service.ts
+++ spa/src/app/servicios/heroes.service.ts
@@ -59,12 +59,24 @@ export class HeroesService {
         console.log('servicio listo para usar');
      }
 
-     getHeroes(){
+    getHeroes(){
          return this.heroes;
      }
-     getHeroe( idx: string){
+    getHeroe( idx: string){
          return this.heroes[idx];
      }
+
+    buscarHeroes( termino: string){
+        let heroesArr: Heroe[] = [];
+        termino = termino.toLowerCase();
+        for (let heroe of this.heroes){
+            let nombre = heroe.nombre.toLowerCase();
+            if (nombre.indexOf(termino) >= 0) {
+                heroesArr.push( heroe );
+            }
+        }
+        return heroesArr;
+    }
 
 }
 
