512816fc459813288ddeed87a3c476ca146c9627
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-08-23 18:06:13 -0500

    json

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -11,4 +11,13 @@ export class AppComponent {
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario : number = 1234.5;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad:500,
+    direccion:{
+      calle: 'Primera',
+      casa:2
+    }
+  }
 }
