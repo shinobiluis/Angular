8a7b2cdb879cbb9fed36453138da99c321c33c03
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-10-10 13:56:30 -0500

    Se realiza consultas para buscar con un input text

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -18,4 +18,13 @@ export class SpotifyService {
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
+
+  getArtista(termino: string){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCzjzo4I0sgpQX5Mfdl-Rk8x2ltmwml-B4EgBQN57b4VMGJV5tZXswIOZ4t5RLyPm2WyOVyLaYrRH1k4lU'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers });
+    
+  }
 }
