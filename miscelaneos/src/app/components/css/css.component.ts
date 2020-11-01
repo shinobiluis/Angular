135e9335b088e2ce9bb84e8c5ce725c8ad99a69e
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-10-31 18:59:41 -0600

    css a un solo componente

--- miscelaneos/src/app/components/css/css.component.ts
+++ miscelaneos/src/app/components/css/css.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
       css works!
     </p>
   `,
-  styles: [
-  ]
+  styles: [`
+    p{
+      color:red;
+      font-size:20px;
+    }
+  `]
 })
 export class CssComponent implements OnInit {
 
