948c723bfb85e8a3e6080caf261936830ad4af63
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-01 19:40:30 -0600

    Agregamos auth0 al navbar

--- authapp/src/app/app.module.ts
+++ authapp/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { PreciosComponent } from './components/precios/precios.component';
 
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +21,12 @@ import { PreciosComponent } from './components/precios/precios.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'geekluis.us.auth0.com',
+      clientId: 'uHczhgvDmNLNGleThfL8pR6egH8dRWF7'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
