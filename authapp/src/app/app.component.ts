287f966791bf3fd2ad0b29f7bd9490afc2e81f50
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-01 19:48:18 -0600

    informacion del usuario logeado

--- authapp/src/app/app.component.ts
+++ authapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'authapp';
-  constructor() {}
+  constructor(public auth: AuthService) {
+    console.log(auth)
+  }
 }
