f88da224bf8a672a62fe2f2132eb068408f7de3a
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-02 14:22:43 -0600

    Se crea un modelo y controla el formulario del login

---
+++ login-app/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
+    this.usuario.email="luis@gmail.com";
+  }
+
+  onSubmit(){
+    console.log("Formulario enviado")
+    console.log(this.usuario)
+  }
+
+}
