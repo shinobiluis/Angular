a0370ada2dced414e36cadf504af5cdd5d16da70
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-02 21:26:32 -0600

    proyeco con login y token

--- login-app/src/app/pages/login/login.component.ts
+++ login-app/src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: UsuarioModel = new UsuarioModel();
+  recordarme = false;
+
+  constructor( private auth: AuthService,
+               private router: Router ) { }
 
   ngOnInit() {
+    if (localStorage.getItem('email')) {
+      // localStorage.setItem('email', this.usuario.email)
+      this.usuario.email = localStorage.getItem('email');
+      this.recordarme = true;
+    }
   }
 
+  login( form: NgForm ){
+    if (form.invalid) {
+      return;
+    }
+    Swal.fire({
+      allowOutsideClick: false, 
+      icon: 'info', 
+      text: 'Espera por Favor..'
+    }); 
+    Swal.showLoading();
+    // console.log(this.usuario)
+    // console.log(form)
+    this.auth.login( this.usuario )
+      .subscribe( resp => {
+        console.log(resp)
+        Swal.close();
+        if (this.recordarme) {
+          localStorage.setItem('email', this.usuario.email)
+        }
+        this.router.navigateByUrl('/home');
+      }, ( err ) => {
+        console.log(err.error.error.message)
+        Swal.fire({
+          allowOutsideClick: false, 
+          icon: 'error', 
+          text: err.error.error.message,
+          title: "Error al autenticar"
+        }); 
+      });
+  }
 }
--- login-app/src/app/pages/registro/registro.component.ts
+++ login-app/src/app/pages/registro/registro.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -9,17 +13,46 @@ import { UsuarioModel } from 'src/app/models/usuario.model';
 export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
+  recordarme = false;
 
-  constructor() { }
+  constructor( private auth: AuthService,
+               private router: Router ) { }
 
   ngOnInit() { 
     this.usuario = new UsuarioModel();
-    this.usuario.email="luis@gmail.com";
   }
 
-  onSubmit(){
-    console.log("Formulario enviado")
-    console.log(this.usuario)
+  onSubmit( form: NgForm ){
+    if (form.invalid) {
+      return;
+    }
+    Swal.fire({
+      allowOutsideClick: false, 
+      icon: 'info', 
+      text: 'Espera por Favor..'
+    }); 
+    Swal.showLoading();
+    // console.log("Formulario enviado")
+    // console.log(this.usuario)
+    // console.log(form)
+    this.auth.nuevoUsuario( this.usuario )
+      .subscribe( resp => {
+        console.log(resp)
+        Swal.close();
+        if (this.recordarme) {
+          localStorage.setItem('email', this.usuario.email)
+        }
+        this.router.navigateByUrl('/home');
+      }, ( err ) => {
+        console.log(err.error.error.message)
+        Swal.fire({
+          allowOutsideClick: false, 
+          icon: 'error', 
+          text: err.error.error.message,
+          title: "Error"
+        }); 
+      });
+    
   }
 
 }
