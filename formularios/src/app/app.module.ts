c2ec06f43b630a5dd13f29f18ee12d1e81583341
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-07 15:33:59 -0600

    Se creo la base de los formularios reactivos y de template

--- formularios/src/app/app.module.ts
+++ formularios/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { RecativeComponent } from './pages/recative/recative.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    RecativeComponent
   ],
   imports: [
     BrowserModule,
---
+++ formularios/src/app/pages/recative/recative.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recative',
+  templateUrl: './recative.component.html',
+  styleUrls: ['./recative.component.css']
+})
+export class RecativeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
