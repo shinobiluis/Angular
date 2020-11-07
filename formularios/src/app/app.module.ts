1745306b5fb79ac508fad82d12e21fa276fad981
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-07 15:41:14 -0600

    Hacemos que angular tome el control de los formularios con FormsModule

--- formularios/src/app/app.module.ts
+++ formularios/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './pages/template/template.component';
@@ -14,7 +16,8 @@ import { RecativeComponent } from './pages/recative/recative.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
