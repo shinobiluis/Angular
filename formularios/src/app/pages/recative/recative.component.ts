e692285f094ab26e8bdb4cd20ea8d530240a0baa
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-07 19:28:39 -0600

    Validaciones html

--- formularios/src/app/pages/recative/recative.component.ts
+++ formularios/src/app/pages/recative/recative.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recative',
@@ -17,15 +17,30 @@ export class RecativeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+  get apellidoNoValido(){
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+  get correoNoValido(){
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
   crearFormulario(){
     this.forma = this.fb.group({
-      nombre:[''],
-      apellido:[''],
-      correo: ['']
+      nombre:['', [ Validators.required, Validators.minLength(4) ] ],
+      apellido:['', Validators.required],
+      correo: ['', [ Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required ] ]
     })
   }
 
   guardar(){
+    if( this.forma.invalid ){
+      return Object.values( this.forma.controls ).forEach( control => {
+        control.markAsTouched();
+      });
+    }
     console.log(this.forma)
   }
 }
