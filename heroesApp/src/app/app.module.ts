bedbdae4b3588d92436199aa2b2807f6c5b7bab1
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-21 17:15:17 -0600

    Se agrego paginas de heroes y heroe con sus links

--- heroesApp/src/app/app.module.ts
+++ heroesApp/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
