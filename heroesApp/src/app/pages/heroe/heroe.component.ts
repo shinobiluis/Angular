df179762a3b510fc6d46c6060d0264cfb7b6bd50
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-21 18:05:45 -0600

    Creamos el modelo del formulario y lo ligamos al mismo

--- heroesApp/src/app/pages/heroe/heroe.component.ts
+++ heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.models'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
+  heroe = new HeroeModel();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  guardar( form: NgForm ){
+    if ( form.invalid){
+      console.log('Formulario no valido');
+      return;
+    }
+    console.log(form);
+    console.log(this.heroe)
+  }
 }
