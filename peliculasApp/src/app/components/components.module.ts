e82a82bd427b3ece1ea462879583a37b74779674
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-11-29 15:10:36 -0600

    creacion de rutas

--- peliculasApp/src/app/components/components.module.ts
+++ peliculasApp/src/app/components/components.module.ts
@@ -1,12 +1,18 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
+  exports:[
+    NavbarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class ComponentsModule { }
